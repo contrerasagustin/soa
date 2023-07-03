42c76c92aef4df9644d04d09d3d296a0eb3455e4
MicaPalau <micaelapalau10@gmail.com>
2023-07-03 20:21:04 -0300

    en clase

--- smart_trafficlight_v2/app/src/main/java/com/proyect/smart_trafficlight/LightActivity.java
+++ smart_trafficlight_v2/app/src/main/java/com/proyect/smart_trafficlight/LightActivity.java
@@ -8,10 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,14 +23,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class LightActivity extends AppCompatActivity {
 
     private ImageView trafficLight_A;
     private ImageView trafficLight_B;
     private static String address = null;
+
+    Handler bluetoothIn;
+    final int handlerState = 0;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    TextView statusSensorA,statusSensorB;
+
+    private ConnectedThread MyConexionBT;
+    // Identificador unico de servicio - SPP UUID
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     private Map<String, Integer> lights = new HashMap<>();
 
@@ -37,6 +53,33 @@ public class LightActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
 
+        //noinspection deprecation
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+                    char MyCar = (char) msg.obj;
+
+                    if (MyCar == 'V') { //Sem a en verde
+                       // tvtMsg.setText("VERDE");
+                        statusSensorA.setText("SI");
+                        statusSensorB.setText("NO");
+                    }
+
+                    if (MyCar == 'R') {
+                        //tvtMsg.setText("ROJO");
+                        statusSensorA.setText("NO");
+                        statusSensorB.setText("SI");
+                    }
+                }
+            }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        verifyStateBT();
+
+
+        statusSensorA = findViewById(R.id.statusSensorA);
+        statusSensorB = findViewById(R.id.statusSensorB);
         trafficLight_A = findViewById(R.id.trafficLight_A);
         trafficLight_B = findViewById(R.id.trafficLight_B);
 
@@ -66,7 +109,7 @@ public class LightActivity extends AppCompatActivity {
 
 
         ///
-        String estadosPalno = "R.Y.T.F";
+        /*String estadosPalno = "R.Y.T.F";
         String[] estados = estadosPalno.split("\\.");
 
         TextView statusSensorA = findViewById(R.id.statusSensorA);
@@ -82,7 +125,7 @@ public class LightActivity extends AppCompatActivity {
             statusSensorB.setText("SI");
 
         trafficLight_A.setImageResource(lights.get(estados[0]));
-        trafficLight_B.setImageResource(lights.get(estados[1]));
+        trafficLight_B.setImageResource(lights.get(estados[1]));*/
         ///
 
     }
@@ -93,7 +136,7 @@ public class LightActivity extends AppCompatActivity {
         Intent intent = getIntent();
         address = intent.getStringExtra(EXTRA_DEVICE_ADDRESS);
         //Setea la direccion MAC
-        /*BluetoothDevice device = btAdapter.getRemoteDevice(address);
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
 
         try {
             btSocket = createBluetoothSocket(device);
@@ -123,8 +166,98 @@ public class LightActivity extends AppCompatActivity {
             } catch (IOException e2) {
             }
         }
-        MyConexionBT = new MainActivity.ConnectedThread(btSocket);
-        MyConexionBT.start();*/
+        MyConexionBT = new ConnectedThread(btSocket);
+        MyConexionBT.start();
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+        }
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try { // Cuando se sale de la aplicación esta parte permite que no se deje abierto el socket
+            btSocket.close();
+        } catch (IOException e2) {
+        }
+    }
+
+    //Comprueba que el dispositivo Bluetooth
+    //está disponible y solicita que se active si está desactivado
+    @SuppressWarnings("deprecation")
+    private void verifyStateBT() {
+
+        if (btAdapter == null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    startActivityForResult(enableBtIntent, 1);
+                    //return;
+                }
+
+            }
+        }
+    }
+    private class ConnectedThread extends Thread
+    {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try
+            {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run()
+        {
+            byte[] byte_in = new byte[1];
+            // Se mantiene en modo escucha para determinar el ingreso de datos
+            while (true) {
+                try {
+                    mmInStream.read(byte_in);
+                    char ch = (char) byte_in[0];
+                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //Envio de trama
+        public void write(String input)
+        {
+            try {
+                mmOutStream.write(input.getBytes());
+            }
+            catch (IOException e)
+            {
+                //si no es posible enviar datos se cierra la conexión
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 
 }
--- smart_trafficlight_v2/app/src/main/java/com/proyect/smart_trafficlight/MainActivity.java
+++ smart_trafficlight_v2/app/src/main/java/com/proyect/smart_trafficlight/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton btnSend,btnSendDefault;
     Button btnDisconnect,enableSmartButton,disableSmartButton,viewSemaphores,backMenu;
     TextView tvtMsg;
+    private StringBuilder recDataString = new StringBuilder();
 
     //-------------------------------------------
     Handler bluetoothIn;
@@ -55,16 +57,53 @@ public class MainActivity extends AppCompatActivity {
         //noinspection deprecation
         bluetoothIn = new Handler() {
             public void handleMessage(android.os.Message msg) {
-                if (msg.what == handlerState) {
-                    char MyCar = (char) msg.obj;
 
-                    if (MyCar == 'S') {
+                String TAG = "handlerState";
+                if (msg.what == handlerState) {
+                    /*char MyCar = (char) msg.obj;
+                    if (MyCar == 'V') {
                         tvtMsg.setText("VERDE");
                     }
 
-                    if (MyCar == 'N') {
+                    if (MyCar == 'R') {
                         tvtMsg.setText("ROJO");
+                    }*/
+
+                    String readMessage = (String) msg.obj;
+                    recDataString.append(readMessage);
+                    int endOfLineIndex = recDataString.indexOf("~");
+                    Log.i(TAG,readMessage);
+                    //cuando recibo toda una linea la muestro en el layout
+                    if (endOfLineIndex > 0)
+                    {
+                        String dataInPrint = recDataString.substring(0, endOfLineIndex);
+                        //tvtMsg.setText(readMessage);
+
+                        if (dataInPrint.equals("V")) {
+                            tvtMsg.setText("VERDE");
+                        }
+
+                        if (dataInPrint.equals("R")) {
+                            tvtMsg.setText("ROJO");
+                        }
+
+
+                        //recDataString.delete(0, recDataString.length());
+                        dataInPrint = " ";
                     }
+                    recDataString.delete(0, recDataString.length());      //clear all string data
+
+
+
+                    /*if (readMessage == "V") {
+                        tvtMsg.setText("VERDE");
+                    }
+
+                    if (readMessage == "R") {
+                        tvtMsg.setText("ROJO");
+                    }*/
+
+
                 }
             }
         };
@@ -243,13 +282,25 @@ public class MainActivity extends AppCompatActivity {
 
         public void run()
         {
-            byte[] byte_in = new byte[1];
+            //byte[] byte_in = new byte[1];
+            byte[] buffer = new byte[256];
+            int bytes;
             // Se mantiene en modo escucha para determinar el ingreso de datos
             while (true) {
                 try {
-                    mmInStream.read(byte_in);
+                    /*mmInStream.read(byte_in);
                     char ch = (char) byte_in[0];
-                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();
+                    bluetoothIn.obtainMessage(handlerState, ch).sendToTarget();*/
+
+                    //se leen los datos del Bluethoot
+                    bytes = mmInStream.read(buffer);
+                    String readMessage = new String(buffer, 0, bytes);
+
+                    //se muestran en el layout de la activity, utilizando el handler del hilo
+                    // principal antes mencionado
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+
+
                 } catch (IOException e) {
                     break;
                 }
